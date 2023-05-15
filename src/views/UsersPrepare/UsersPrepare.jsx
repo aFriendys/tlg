3404c812cec0bb40d623b70577886f0fca864e63
aFriend <hsphsphsp@yandex.ru>
2023-05-15 23:41:42 +0300

    123

--- src/views/UsersPrepare/UsersPrepare.jsx
+++ src/views/UsersPrepare/UsersPrepare.jsx
@@ -29,7 +29,7 @@ export function UsersPrepare () {
         sendMessages()
       }
     }
-  }, [])
+  }, [users])
 
   const debouncedHandler = debounce((callback, value) => { dispatch(callback(value)) }, 300)
 
