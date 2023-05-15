cc0af3046a3b69f46933b2f6bb79fd8b4ccff547
aFriend <hsphsphsp@yandex.ru>
2023-05-15 22:52:55 +0300

    1st

---
+++ src/views/UsersPrepare/UsersPrepare.jsx
@@ -0,0 +1,65 @@
+import styles from './UsersPrepare.module.scss'
+
+import { setUsers, setMessage, setQuery, pushUsersDone, resetUsersDone, shiftUser } from '../../store/appSlice'
+import { debounce } from 'lodash'
+
+import { Input, Typography, Button } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { useNavigate } from 'react-router-dom'
+import { telegramClient } from '../../api'
+import { useEffect } from 'react'
+const { TextArea } = Input
+const { Text } = Typography
+
+export function UsersPrepare () {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const query = useSelector(state => state.appSlice.query)
+  const users = useSelector(state => state.appSlice.users)
+  const message = useSelector(state => state.appSlice.message)
+  const usersIsFetching = useSelector(state => state.appSlice.usersIsFetching)
+
+  const debouncedHandler = debounce((callback, value) => { dispatch(callback(value)) }, 300)
+
+  function onUsersChangeHandler (e) {
+    debouncedHandler(setMessage, e.target.value)
+  }
+
+  function onQueryChangeHandler (e) {
+    debouncedHandler(setQuery, e.target.value)
+  }
+
+  useEffect()
+  if (users.length) {
+    navigate('/inProgress')
+    sendMessages()
+  }
+
+  async function sendMessage (user) {
+    dispatch(shiftUser())
+    await telegramClient.sendMessage(user, message)
+    dispatch(pushUsersDone(user))
+  }
+
+  async function sendMessages () {
+    dispatch(resetUsersDone())
+    for (const user of users) {
+      await sendMessage(user)
+    }
+  }
+
+  return (
+    <section className={styles.section}>
+      <h2>Finally, fill remaining fields</h2>
+      <div className={styles.inputWrapper}>
+        <TextArea placeholder="Message" autoSize={{ minRows: 2, maxRows: 6 }} onChange={onUsersChangeHandler} />
+        <div className={styles.inputWrapperUsers}>
+        <TextArea placeholder="Users / Channels" autoSize={{ minRows: 2, maxRows: 6 }} onChange={onQueryChangeHandler} />
+        <Text type="danger">*Channels must be marked with :channel suffix, for example @name:channel</Text>
+        </div>
+        <Button type='primary' onClick={() => dispatch(setUsers(query))} disabled={usersIsFetching} loading={usersIsFetching}>Send messages</Button>
+      </div>
+    </section>
+  )
+}
